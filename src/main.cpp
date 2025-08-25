3e36f34ff0bb164f5c05b4701b67e8a0a58b4bb5
dehroox <dehrexd@gmail.com>
2025-08-25 20:42:24 +0800

    refactoring, and optimization

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <QMetaObject>
 #include <QPixmap>
 #include <atomic>
-#include <iostream>
 #include <thread>
 #include <vector>
 
@@ -23,7 +22,6 @@ auto main(int argc, char** argv) -> int {
 
     FrameDimensions frame_dimensions =
         V4l2Device::select_highest_resolution(device.file_descriptor);
-
     if (!V4l2Device::configure_video_format(device.file_descriptor,
                                             frame_dimensions)) {
         V4l2Device::close_device(device.file_descriptor);
@@ -37,15 +35,20 @@ auto main(int argc, char** argv) -> int {
     }
 
     if (!V4l2Device::start_video_stream(device.file_descriptor)) {
-        std::cerr << "VIDIOC_STREAMON failed\n";
         V4l2Device::unmap_buffers(device);
         V4l2Device::close_device(device.file_descriptor);
         return 1;
     }
 
+    const int frame_bytes = static_cast<int>(frame_dimensions.width) *
+                            static_cast<int>(frame_dimensions.height) *
+                            static_cast<int>(Constants::K_RGB_COMPONENTS);
     std::vector<unsigned char> rgb_frame_buffer(
-        static_cast<size_t>(frame_dimensions.width * frame_dimensions.height *
-                            Constants::K_RGB_COMPONENTS));
+        static_cast<size_t>(frame_bytes), 0);
+
+    QImage frame_image(
+        rgb_frame_buffer.data(), static_cast<int>(frame_dimensions.width),
+        static_cast<int>(frame_dimensions.height), QImage::Format_RGB888);
 
     QLabel display_label;
     display_label.resize(static_cast<int>(frame_dimensions.width),
@@ -53,12 +56,16 @@ auto main(int argc, char** argv) -> int {
     display_label.show();
 
     std::atomic<bool> running{true};
-    std::thread capture_thread([&]() {
+    std::thread capture_thread([&device, &display_label, &rgb_frame_buffer,
+                                &frame_dimensions, &running]() {
         v4l2_buffer buffer{};
         buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buffer.memory = V4L2_MEMORY_MMAP;
 
-        while (running) {
+        const int width_int = static_cast<int>(frame_dimensions.width);
+        const int height_int = static_cast<int>(frame_dimensions.height);
+
+        while (running.load(std::memory_order_relaxed)) {
             if (Utils::continually_retry_ioctl(device.file_descriptor,
                                                VIDIOC_DQBUF, &buffer) == -1) {
                 continue;
@@ -66,19 +73,18 @@ auto main(int argc, char** argv) -> int {
 
             convert_yuyv_to_rgb(
                 static_cast<unsigned char*>(
-                    device.mapped_buffers.at(buffer.index).start_address),
+                    device.mapped_buffers.at(static_cast<size_t>(buffer.index))
+                        .start_address),
                 rgb_frame_buffer.data(), frame_dimensions);
 
             QMetaObject::invokeMethod(
                 &display_label,
-                [&]() {
-                    QImage frame_image(
-                        rgb_frame_buffer.data(),
-                        static_cast<int>(frame_dimensions.width),
-                        static_cast<int>(frame_dimensions.height),
-                        QImage::Format_RGB888);
+                [ptr = rgb_frame_buffer.data(), width_copy = width_int,
+                 height_copy = height_int, &display_label]() {
+                    QImage tmp_image(ptr, width_copy, height_copy,
+                                     QImage::Format_RGB888);
                     display_label.setPixmap(
-                        QPixmap::fromImage(frame_image.mirrored(true, false)));
+                        QPixmap::fromImage(tmp_image.mirrored(true, false)));
                 },
                 Qt::QueuedConnection);
 
@@ -88,7 +94,6 @@ auto main(int argc, char** argv) -> int {
     });
 
     int exit_code = QApplication::exec();
-
     running = false;
     capture_thread.join();
 
--- src/v4l2_device.cpp
+++ src/v4l2_device.cpp
@@ -4,8 +4,11 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cerrno>
 #include <iostream>
+#include <thread>
+#include <vector>
 
 #include "hand_music.hpp"
 
@@ -15,7 +18,6 @@ auto open(const char* device_path) -> int {
     const int file_descriptor = ::open(device_path, O_RDWR);
     if (file_descriptor == -1) {
         std::cerr << "Failed to open video device " << device_path << "\n";
-        return -1;
     }
     return file_descriptor;
 }
@@ -25,37 +27,28 @@ void close_device(int file_descriptor) { ::close(file_descriptor); }
 auto select_highest_resolution(int video_file_descriptor) -> FrameDimensions {
     FrameDimensions frame_dimensions{
         .width = 0U, .height = 0U, .stride_bytes = 0U};
+    v4l2_frmsizeenum frame_size{};
+    frame_size.index = 0;
+    frame_size.pixel_format = V4L2_PIX_FMT_YUYV;
 
-    v4l2_fmtdesc format_description{};
-    format_description.index = 0;
-    format_description.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-
-    while (ioctl(video_file_descriptor, VIDIOC_ENUM_FMT, &format_description) !=
+    while (ioctl(video_file_descriptor, VIDIOC_ENUM_FRAMESIZES, &frame_size) !=
            -1) {
-        if (format_description.pixelformat != V4L2_PIX_FMT_YUYV) {
-            ++format_description.index;
-            continue;
+        if (frame_size.type == V4L2_FRMSIZE_TYPE_STEPWISE) {
+            frame_dimensions.width = frame_size.stepwise.max_width;
+            frame_dimensions.height = frame_size.stepwise.max_height;
+            break;
         }
-
-        v4l2_frmsizeenum frame_size{};
-        frame_size.index = 0;
-        frame_size.pixel_format = format_description.pixelformat;
-
-        while (ioctl(video_file_descriptor, VIDIOC_ENUM_FRAMESIZES,
-                     &frame_size) != -1) {
-            if (frame_size.type == V4L2_FRMSIZE_TYPE_DISCRETE) {
-                if (frame_size.discrete.width * frame_size.discrete.height >
-                    frame_dimensions.width * frame_dimensions.height) {
-                    frame_dimensions.width = frame_size.discrete.width;
-                    frame_dimensions.height = frame_size.discrete.height;
-                }
-            } else if (frame_size.type == V4L2_FRMSIZE_TYPE_STEPWISE) {
-                frame_dimensions.width = frame_size.stepwise.max_width;
-                frame_dimensions.height = frame_size.stepwise.max_height;
+        if (frame_size.type == V4L2_FRMSIZE_TYPE_DISCRETE) {
+            uint32_t area =
+                frame_size.discrete.width * frame_size.discrete.height;
+            uint32_t current_area =
+                frame_dimensions.width * frame_dimensions.height;
+            if (area > current_area) {
+                frame_dimensions.width = frame_size.discrete.width;
+                frame_dimensions.height = frame_size.discrete.height;
             }
-            ++frame_size.index;
         }
-        ++format_description.index;
+        ++frame_size.index;
     }
 
     std::cout << "Using highest supported resolution: "
@@ -79,69 +72,73 @@ auto configure_video_format(int video_file_descriptor,
 
     if (Utils::continually_retry_ioctl(video_file_descriptor, VIDIOC_S_FMT,
                                        &video_format) == -1) {
-        std::cerr << "VIDIOC_S_FMT failed\n";
         return false;
     }
-
     frame_dimensions.stride_bytes = video_format.fmt.pix.bytesperline;
     return true;
 }
 
 auto setup_memory_mapped_buffers(Device& device, unsigned int buffer_count)
     -> bool {
+    int file_descriptor = device.file_descriptor;
     v4l2_requestbuffers request_buffers{};
     request_buffers.count = buffer_count;
     request_buffers.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     request_buffers.memory = V4L2_MEMORY_MMAP;
 
-    if (Utils::continually_retry_ioctl(device.file_descriptor, VIDIOC_REQBUFS,
+    if (Utils::continually_retry_ioctl(file_descriptor, VIDIOC_REQBUFS,
                                        &request_buffers) == -1 ||
         request_buffers.count < 2) {
-        std::cerr << "VIDIOC_REQBUFS failed\n";
         return false;
     }
 
     device.buffer_count = request_buffers.count;
+    auto& mapped_buffers_ref = device.mapped_buffers;
 
-    for (unsigned int buffer_index = 0; buffer_index < device.buffer_count;
-         ++buffer_index) {
+    auto map_buffer = [&](unsigned int buffer_index) -> bool {
         v4l2_buffer buffer{};
         buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buffer.memory = V4L2_MEMORY_MMAP;
         buffer.index = buffer_index;
 
-        if (Utils::continually_retry_ioctl(device.file_descriptor,
-                                           VIDIOC_QUERYBUF, &buffer) == -1) {
-            std::cerr << "VIDIOC_QUERYBUF failed\n";
-            return false;
-        }
-
-        device.mapped_buffers.at(buffer.index).start_address =
-            mmap(nullptr, buffer.length, PROT_READ | PROT_WRITE, MAP_SHARED,
-                 device.file_descriptor, buffer.m.offset);
-
-        if (device.mapped_buffers.at(buffer.index).start_address ==
-            MAP_FAILED) {
-            std::cerr << "Memory mapping failed\n";
-            return false;
-        }
-
-        device.mapped_buffers.at(buffer.index).length_bytes = buffer.length;
-
-        if (Utils::continually_retry_ioctl(device.file_descriptor, VIDIOC_QBUF,
+        if (Utils::continually_retry_ioctl(file_descriptor, VIDIOC_QUERYBUF,
                                            &buffer) == -1) {
-            std::cerr << "VIDIOC_QBUF failed\n";
             return false;
         }
+
+        mapped_buffers_ref.at(buffer_index).start_address =
+            mmap(nullptr, buffer.length, PROT_READ | PROT_WRITE, MAP_SHARED,
+                 file_descriptor, buffer.m.offset);
+        if (mapped_buffers_ref.at(buffer_index).start_address == MAP_FAILED) {
+            return false;
+        }
+
+        mapped_buffers_ref.at(buffer_index).length_bytes = buffer.length;
+
+        return Utils::continually_retry_ioctl(file_descriptor, VIDIOC_QBUF,
+                                              &buffer) != -1;
+    };
+
+    std::vector<std::thread> threads;
+    std::vector<bool> results(device.buffer_count, false);
+
+    threads.reserve(device.buffer_count);
+    for (unsigned int i = 0; i < device.buffer_count; ++i) {
+        threads.emplace_back([&, i] { results[i] = map_buffer(i); });
     }
 
-    return true;
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    return std::ranges::all_of(results, [](bool success) { return success; });
 }
 
 void unmap_buffers(Device& device) {
+    auto& mapped_buffers_ref = device.mapped_buffers;
     for (unsigned int i = 0; i < device.buffer_count; ++i) {
-        munmap(device.mapped_buffers.at(i).start_address,
-               device.mapped_buffers.at(i).length_bytes);
+        munmap(mapped_buffers_ref.at(i).start_address,
+               mapped_buffers_ref.at(i).length_bytes);
     }
 }
 
