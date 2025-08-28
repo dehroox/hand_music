0d7898e1c1cf9ed414c9170df71bb5666f81d38a
dehroox <dehrexd@gmail.com>
2025-08-28 11:20:08 +0800

    s

--- src/frontend/include/frontend_manager.h
+++ src/frontend/include/frontend_manager.h
@@ -3,24 +3,20 @@
 
 #include <stdbool.h>
 
-#include "../../common/common_types.h"
+#include "common_types.h"
 #include "x11_utils.h"
 
-// Define a context struct for the Frontend module
 typedef struct {
     X11Context x11_context;
     const FrameDimensions *frame_dimensions;
-} FrontendContext;
+} __attribute__((aligned(64))) FrontendContext;
 
-// Function to initialize the Frontend module
 bool Frontend_init(FrontendContext *context,
                    const FrameDimensions *frame_dimensions,
                    unsigned char *rgb_flipped_buffer);
 
-// Function to run the Frontend event loop
 void Frontend_run(FrontendContext *context, _Atomic bool *running_flag);
 
-// Function to clean up the Frontend module
 void Frontend_cleanup(FrontendContext *context);
 
 #endif  // FRONTEND_MANAGER_H
